90896d5371def6148f90d65f92c83a6634616d97
Jean Coupon <jean.coupon@gmail.com>
2020-08-06 19:04:14 +0200

    ex 4.1

--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,7 +1,32 @@
 #include "test_framework/generic_test.h"
+#include <iostream>
+
+using namespace std;
+
+
+bool getBit(unsigned long long num, int i){
+
+    unsigned long long one = 1, res;
+
+    res = num & (one << i);
+
+    return res != 0;
+}
+
 short Parity(unsigned long long x) {
   // TODO - you fill in here.
-  return 0;
+
+  int n = 0;
+  for (int i=0; i<64; i++){
+    n += getBit(x, i);
+    // cout << i << " " << n << endl;
+  }
+
+  cout << n << endl;
+
+  if(n%2 == 0) return 0;
+  
+  return 1;
 }
 
 int main(int argc, char* argv[]) {
