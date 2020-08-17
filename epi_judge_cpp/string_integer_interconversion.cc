6f713a92746212f2b75c18d4f1ac8d7bb1ff7906
Jean Coupon <jean.coupon@gmail.com>
2020-08-17 10:19:50 +0200

    ex 6.1

--- epi_judge_cpp/string_integer_interconversion.cc
+++ epi_judge_cpp/string_integer_interconversion.cc
@@ -4,14 +4,58 @@
 #include "test_framework/test_failure.h"
 using std::string;
 
+
+using namespace std;
+
 string IntToString(int x) {
-  // TODO - you fill in here.
-  return "0";
+  
+  if(x == 0) return "0";
+  if(x == INT32_MIN) return "-2147483648";
+
+  long r;
+  long m=10;
+  string result = "";
+  char sign = '+';
+
+
+  if(x<0){
+    sign = '-';
+    x *= -1;
+  }
+
+  while(x!=0){
+
+    r = x%m;
+    x -= r;
+    result += r/(m/10)+'0';
+    m *= 10;
+  }
+  if(sign=='-'){
+    result += sign;
+  }
+  reverse(result.begin(), result.end());
+
+  return result;
 }
+
 int StringToInt(const string& s) {
-  // TODO - you fill in here.
-  return 0;
+ 
+  int sign = +1;
+  if(s[0] == '-') sign = -1;
+
+  int multiple = 1;
+  int result = 0;
+
+  for (int i=s.size()-1;i>=0;i--){
+    if('0' <= s[i] && s[i] <= '9'){
+      result += (s[i]-'0')*multiple;
+      multiple *= 10;
+    }
+  }
+
+  return sign*result;
 }
+
 void Wrapper(int x, const string& s) {
   if (stoi(IntToString(x)) != x) {
     throw TestFailure("Int to string conversion failed");
@@ -25,6 +69,7 @@ void Wrapper(int x, const string& s) {
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x", "s"};
+
   return GenericTestMain(args, "string_integer_interconversion.cc",
                          "string_integer_interconversion.tsv", &Wrapper,
                          DefaultComparator{}, param_names);
