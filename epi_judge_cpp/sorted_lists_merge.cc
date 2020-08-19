57aef99ee228e62485b87c8cd78f1ec095fa7b9b
Jean Coupon <jean.coupon@gmail.com>
2020-08-19 13:28:47 +0200

    ex 7.1

--- epi_judge_cpp/sorted_lists_merge.cc
+++ epi_judge_cpp/sorted_lists_merge.cc
@@ -2,8 +2,25 @@
 #include "test_framework/generic_test.h"
 shared_ptr<ListNode<int>> MergeTwoSortedLists(shared_ptr<ListNode<int>> L1,
                                               shared_ptr<ListNode<int>> L2) {
-  // TODO - you fill in here.
-  return nullptr;
+  
+  shared_ptr<ListNode<int>> head(new ListNode<int>);
+  auto tail = head;
+
+  while(L1 && L2){
+    if(L1->data <= L2->data){
+      tail->next = L1;
+      L1 = L1->next;
+    }
+    else{
+      tail->next = L2;
+      L2 = L2->next;
+    }
+    tail = tail->next;
+   }
+
+  tail->next = L1 ? L1 : L2;
+
+  return head->next;
 }
 
 int main(int argc, char* argv[]) {
