98fe7675d90fc3f3ecb7ee473c546eb76c1e710a
Jean Coupon <jean.coupon@gmail.com>
2021-01-13 17:52:50 +0100

    sorted arrays merge

--- epi_judge_cpp/sorted_arrays_merge.cc
+++ epi_judge_cpp/sorted_arrays_merge.cc
@@ -1,10 +1,36 @@
 #include <vector>
+#include <queue>
 
 #include "test_framework/generic_test.h"
 using std::vector;
+
 vector<int> MergeSortedArrays(const vector<vector<int>>& sorted_arrays) {
   // TODO - you fill in here.
-  return {};
+
+  //priority_queue<vector<int>, vector<vector<int>>> minHeap;
+  std::priority_queue<vector<int>, vector<vector<int>>, std::greater<vector<int>>> minHeap;
+
+  for(int i=0; i<sorted_arrays.size();++i){
+    if(sorted_arrays[i].size() > 0)
+      minHeap.push({sorted_arrays[i][0], 0, i});
+  }
+
+  vector<int> result;
+  while(!minHeap.empty()){
+    int minVal = minHeap.top()[0];
+    int minIdx = minHeap.top()[1];
+    int minArrayIdx = minHeap.top()[2];
+
+    minHeap.pop();
+
+    result.push_back(minVal);
+    if( minIdx +1 < sorted_arrays[minArrayIdx].size()){
+      minHeap.push({sorted_arrays[minArrayIdx][minIdx+1], minIdx+1, minArrayIdx});
+    }
+
+  }
+
+  return result;
 }
 
 int main(int argc, char* argv[]) {
