f517d9275355986a73ecea7da4a31b85646cd6c9
Jean Coupon <jean.coupon@gmail.com>
2021-01-17 18:28:04 +0100

    12.1

--- epi_judge_cpp/is_string_permutable_to_palindrome.cc
+++ epi_judge_cpp/is_string_permutable_to_palindrome.cc
@@ -1,11 +1,31 @@
 #include <string>
+#include <unordered_map>
 
 #include "test_framework/generic_test.h"
 using std::string;
+using namespace std;
 
 bool CanFormPalindrome(const string& s) {
   // TODO - you fill in here.
-  return true;
+
+  unordered_map<char, int> letterMap;
+
+  for(auto c:s){
+    letterMap[c]++;
+  }
+
+  int nOdd = 0;
+  for(auto c:letterMap){
+    nOdd += c.second%2 != 0;
+  }
+  return nOdd <= 1; // odd string
+
+  if(s.size()%2 == 0){
+    return nOdd == 0; // even string
+  }
+  else{
+  }
+
 }
 
 int main(int argc, char* argv[]) {
