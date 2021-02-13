4a8f854b93071a5f757f75b6ac78ffbba1165a90
Jean Coupon <jean.coupon@gmail.com>
2021-02-13 18:02:55 +0100

    ex 7.2

--- epi_judge_cpp/reverse_sublist.cc
+++ epi_judge_cpp/reverse_sublist.cc
@@ -1,10 +1,46 @@
 #include "list_node.h"
 #include "test_framework/generic_test.h"
 
+shared_ptr<ListNode<int>> ReverseListN(shared_ptr<ListNode<int>> head, int n) {
+
+    auto current = head;
+    shared_ptr<ListNode<int>> prev = nullptr;
+
+    int k = 0;
+    while(current && k++ <= n){
+        auto tmp = current->next;
+        current->next = prev;
+        prev = current;
+        current = tmp;
+    }
+
+    head->next = current;
+
+    return prev;
+}
+
+
 shared_ptr<ListNode<int>> ReverseSublist(shared_ptr<ListNode<int>> L, int start,
                                          int finish) {
   // TODO - you fill in here.
-  return nullptr;
+
+    if(start == 0)
+        return L;
+
+    shared_ptr<ListNode<int>> head = L;
+
+    if(start == 1){
+        return ReverseListN(head, finish-start);
+    }else{
+        int n = 2;
+        while(n++ < start){
+            head = head->next;
+        }
+        head->next = ReverseListN(head->next, finish-start);
+
+        return L;
+    }
+
 }
 
 int main(int argc, char* argv[]) {
